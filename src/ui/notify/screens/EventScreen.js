b01e2819de9d19361787c8e5a5a212795e1d66de
Rimoosoo <50680955+Rimoosoo@users.noreply.github.com>
2023-03-08 12:33:14 +0900

    commit

--- src/ui/notify/screens/EventScreen.js
+++ src/ui/notify/screens/EventScreen.js
@@ -1,23 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Pressable, Linking } from "react-native";
 import Colors from "../../../../assets/Colors";
 import { Entypo } from "@expo/vector-icons";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 function EventScreen() {
+  const start = useSharedValue({ x: 0, y: 0 });
+  const [height, setHeight] = useState(start.y);
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({ y: 150 });
+  const animatedStyle = useAnimatedStyle(() => {
+    let translateYValue = offset.value.y;
+    if (translateYValue < 0) {
+      translateYValue = 0;
+    }
+    return {
+      backgroundColor: isPressed.value ? "yellow" : "blue",
+      height: withSpring(translateYValue, {
+        damping: 15,
+        stiffness: 150,
+        restSpeedThreshold: 0.1,
+        restDisplacementThreshold: 0.1,
+      }),
+    };
+  });
+
+  console.log(start.value.y);
+
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate((e) => {
+      console.log(e.translationY + start.value.y);
+      offset.value = {
+        y: e.translationY + start.value.y,
+        x: e.translationX + start.value.x,
+      };
+    })
+    .onEnd(() => {
+      start.value = {
+        y: offset.value.y,
+      };
+    })
+    .onFinalize(() => {
+      isPressed.value = false;
+    });
+
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.notifyDragView, animatedStyle]}>
+          <View
+            style={{
+              height: 100,
+              alignItems: "center",
+              justifyContent: "center",
+              height: "50%",
+              borderRadius: 20,
+              backgroundColor: Colors.basicColor.gray,
+              width: "80%",
+              marginVertical: 10,
+            }}
+          >
+            <Text style={{ fontFamily: "gowun-regular" }}>
+              이벤트가 곧 시작됩니다
+            </Text>
+          </View>
+        </Animated.View>
+      </GestureDetector>
       <Pressable
-        style={{
-          position: "absolute",
-          bottom: 10,
-          right: 10,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: Colors.basicColor.gray,
-          padding: 10,
-          borderRadius: 10,
-          zIndex: 1,
-        }}
+        style={styles.notifyAddBtn}
         onPress={() => {
           console.log("clicked");
           Linking.openURL("https://forms.gle/Rw8rduVAd26maDxm8");
@@ -40,7 +102,7 @@ function EventScreen() {
           등록된 이벤트가 없습니다
         </Text>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 export default EventScreen;
@@ -55,9 +117,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.basicColor.magenta,
   },
+  //
+  notifyDragView: {
+    position: "absolute",
+    zIndex: 5,
+    width: "100%",
+    alignItems: "center",
+  },
+  //
+  notifyAddBtn: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.basicColor.gray,
+    padding: 10,
+    borderRadius: 10,
+    zIndex: 1,
+  },
   notifyContentContainer: {
     flex: 10,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
   },
 });
