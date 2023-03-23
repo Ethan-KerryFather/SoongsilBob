c68a1df29f2e24358c6ae4a16290de0e7339c7cc
Rimoosoo <50680955+Rimoosoo@users.noreply.github.com>
2023-03-23 23:57:34 +0900

    commit

--- src/ui/notify/screens/NotifyScreen.js
+++ src/ui/notify/screens/NotifyScreen.js
@@ -1,25 +1,24 @@
-import React from "react";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-  Linking,
-  FlatList,
-  useWindowDimensions,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
 
-function NotifyScreen() {
-  return (
-    <View>
-      <Text>NotifyScreen</Text>
-    </View>
-  );
-}
+const NotifyScreen = () => {
+  const [notice, setNotice] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://scatch.ssu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/?f&category&slug=%EC%88%AD%EC%8B%A4%EB%8C%80%ED%95%99%EA%B5%90-%EC%A7%80%EC%97%AD%EC%A7%80%EB%8A%A5%ED%99%94%ED%98%81%EC%8B%A0%EC%9D%B8%EC%9E%AC%EC%96%91%EC%84%B1%EC%82%AC%EC%97%85-%EC%A0%84%EC%9E%84%EC%97%B0%EA%B5%AC&keyword"
+      )
+      .then((response) => {
+        setNotice(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return <View style={{ flex: 1 }}></View>;
+};
+
 export default NotifyScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- src/ui/roulette/RouletteScreen.js
+++ src/ui/roulette/RouletteScreen.js
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import SlotMachine from "react-native-slot-machine";
 
 function RouletteScreen({ route }) {
   const navigation = useNavigation();
@@ -11,7 +12,47 @@ function RouletteScreen({ route }) {
       headerShown: false,
     });
   }, [navigation]);
-  return <View style={styles.container}></View>;
+
+  const slotRef = useRef(null);
+
+  const startSlot = () => {
+    slotRef.current.spin();
+  };
+
+  return (
+    <View style={styles.container}>
+      <SlotMachine
+        ref={slotRef}
+        initialAnimation={false}
+        textStyles={{ fontSize: 30, color: "white" }}
+        reels={[
+          {
+            values: ["A", "B", "C"],
+            loop: 5,
+            speed: 200,
+            textStyle: { fontWeight: "bold" },
+          },
+          {
+            values: ["1", "2", "3"],
+            loop: 5,
+            speed: 200,
+            textStyle: { fontWeight: "bold" },
+          },
+          {
+            values: ["X", "Y", "Z"],
+            loop: 5,
+            speed: 200,
+            textStyle: { fontWeight: "bold" },
+          },
+        ]}
+        reelConfig={{ tension: 200, friction: 10, mass: 0.5 }}
+        style={{ height: 600 }}
+      />
+      <Pressable onPress={startSlot}>
+        <Text>시작</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 export default RouletteScreen;
@@ -21,6 +62,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
   },
 });
