50b8711af8885f920c0f22f9a018ffefeefcd9d9
Rimoosoo <50680955+Rimoosoo@users.noreply.github.com>
2023-04-17 12:39:50 +0900

    commit

--- src/ui/roulette/RouletteScreen.js
+++ src/ui/roulette/RouletteScreen.js
@@ -1,45 +1,108 @@
 import { useNavigation } from "@react-navigation/native";
-import AnimatedLottieView from "lottie-react-native";
-import React, { useEffect, useRef } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
-//@ts-ignore
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Text, StyleSheet, Pressable, View, ScrollView } from "react-native";
 import SlotMachine from "react-native-slot-machine";
-import { BigTitle } from "../../styled/styledComponents";
-import * as Animatable from "react-native-animatable";
+import stores from "../../resource/stores";
+import Colors from "../../../assets/Colors";
+import { BigTitle, SmallTitle } from "../../styled/styledComponents";
 
-function RouletteScreen({ route }) {
+function RouletteScreen() {
   const navigation = useNavigation();
+  const [names, setNames] = useState("");
+  const [uniqueWords, setUniqueWords] = useState("");
 
+  const [categoryList, setCategoryList] = useState([
+    { name: "한식", isSelected: false },
+    { name: "일식", isSelected: false },
+    { name: "양식", isSelected: false },
+    { name: "아시안", isSelected: false },
+    { name: "술집", isSelected: false },
+    { name: "테이크아웃", isSelected: false },
+    { name: "치킨/피자", isSelected: false },
+    { name: "카페", isSelected: false },
+  ]);
   useEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
+    let temp = "";
+    let name = [];
+    stores.alcohol.forEach((element) => {
+      temp = temp.concat(element.name);
+      name.push(element.name);
+    });
+    // stores.asian.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.western.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.cafe.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.chickenPizza.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.japanese.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.korean.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+    // stores.takeout.forEach((element) => {
+    //   temp = temp.concat(element.name);
+    //   name.push(element.name);
+    // });
+
+    const removedName = temp.replace(/\s/g, "");
+    result = [...new Set(removedName)].join("");
+    console.log(name);
+    setUniqueWords(result);
+    setNames(name);
   }, []);
+
   return (
     <View style={styles.container}>
-      <Animatable.Text
-        animation="slideInDown"
-        iterationCount={5}
-        direction="alternate"
-        style={{
-          fontFamily: "black-sans",
-          fontSize: 30,
-          alignSelf: "center",
-          marginTop: 80,
-          color: "white",
-          zIndex: 3,
-          backgroundColor: "brown",
-          padding: 30,
-          borderRadius: 50,
-        }}
-      >
-        밥집 룰렛 준비중
-      </Animatable.Text>
-      <AnimatedLottieView
-        style={{ flex: 1, borderWidth: 0.5, backgroundColor: "blue" }}
-        source={require("../../../assets/roulette.json")}
-        autoPlay
-      />
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {categoryList.map((element) => {
+          return (
+            <Pressable
+              style={{
+                width: 100,
+                height: 100,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: Colors.basicColor.grayTrans1,
+                margin: 5,
+                borderRadius: 20,
+              }}
+              onPress={() => {
+                console.log(element);
+              }}
+            >
+              <SmallTitle>{element.name}</SmallTitle>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
+
+      <View>
+        <BigTitle>오늘</BigTitle>
+        <SlotMachine
+          text="JM호프바"
+          range={uniqueWords}
+          width={50}
+          height={80}
+        />
+        <BigTitle>어때요</BigTitle>
+      </View>
+      <Text>{uniqueWords}</Text>
     </View>
   );
 }
@@ -49,5 +112,8 @@ export default RouletteScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
 });
